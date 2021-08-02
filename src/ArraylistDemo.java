3253e6bfbb0907d80dd1f0222a1f30c7d5d1dc2a
skundu2000 <swatik2000g@gmail.com>
2021-08-02 21:12:52 +0100

    "ArrayList example: make a colour list"

---
+++ src/ArraylistDemo.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class ArraylistDemo {
+
+	public static void main(String[] args) {
+		// Make a array list of colours
+		
+		ArrayList<String> clr = new ArrayList<String>();
+		clr.add("Red");
+		clr.add("Blue");
+		clr.add("Green");
+		clr.add("Yellow");
+		clr.add("Black");
+		
+		//print the list
+		
+		for(int i=0 ; i<clr.size(); i++) {
+			System.out.println(clr.get(i));
+		}
+		
+
+	}
+
+}
