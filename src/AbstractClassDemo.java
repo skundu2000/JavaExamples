b29462b652411c38e0377e446315109a18268687
skundu2000 <swatik2000g@gmail.com>
2021-08-04 20:42:17 +0100

    "abstract class and abstract method"

---
+++ src/AbstractClassDemo.java
@@ -0,0 +1,30 @@
+abstract class Animal{
+	public abstract void animalType();
+	
+	public static void animalFood(String food) {
+		System.out.println(food);
+	}
+	
+}
+
+ class Cat extends Animal{
+	
+	public  void animalType() {
+		System.out.println("Animal type: Cat");
+		
+	}
+	
+}
+
+
+public class AbstractClassDemo {
+	public static void main(String args[]) {
+		
+		Cat ob = new Cat();
+		ob.animalType();
+		ob.animalFood("salmon");
+		
+		
+	}
+
+}
