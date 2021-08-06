fb29f2099e6ea7a7b4819c8f0c47b4e7fc13841e
skundu2000 <swatik2000g@gmail.com>
2021-08-06 12:06:28 +0100

    "HashMap update"

--- src/HashMapDemo.java
+++ src/HashMapDemo.java
@@ -1,21 +1,42 @@
 import java.util.HashMap;
 
+import java.util.HashMap;
+
 public class HashMapDemo {
 
 	public static void main(String[] args) {
-		
-		//create a HashMap object
-	 HashMap<String , Integer> map = new HashMap<String , Integer>();
-	 
-	 //add key value pair
-	 map.put("John", 30);
-	 map.put("Chris" , 35);
-	 map.put("Mohit" , 50);
-	 
-	 for(String i : map.keySet()) {
-		 System.out.println(i + ":" +map.get(i));
-	 }
-	 
-	}
+		HashMap <String , Integer>ob = new HashMap<String , Integer>();
+		ob.put("Raman", 30);
+		ob.put("Gita", 32);
+		ob.put("Chris", 40);
+		ob.put("John", 28);
 
+		System.out.println(ob);
+		
+		// Remove an item
+		ob.remove("John");
+
+		// how many items
+		System.out.println(ob.size());
+
+		// get the value
+		System.out.println(ob.get("Raman"));
+
+		//to get only keys
+		for(String x : ob.keySet()) {
+			System.out.println(x);
+		}
+
+		//to get only values
+		for(int y : ob.values()) {
+			System.out.println(y);
+		}
+			// to print keys and values
+
+		for(String i : ob.keySet()) {			
+			System.out.println(i +":"+ob.get(i));
+		}								
+			
+		
+	}
 }
